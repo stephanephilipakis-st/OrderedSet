e6341486d71b9a185fc41fabf7042aa12e47e661
Stephane Philipakis <stephane.philipakis@sensortower.com>
2025-06-29 12:04:44 -0700

    Update Package.swift
    testing

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "OrderedSet",
-    products: [.library(name: "OrderedSet", targets: ["OrderedSet"])],
+    products: [.library(name: "OrderedSet", type: .dynamic, targets: ["OrderedSet"])],
     targets: [.target(name: "OrderedSet", path: "Sources")]
 )
